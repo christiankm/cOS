63c37eb4747e5a276349e540e0f6ef164cc3271e
Christian Mitteldorf <christian.km@me.com>
2023-06-24 02:04:04 +1000

    Expand main assembler to print OS name

--- main.asm
+++ main.asm
@@ -10,11 +10,54 @@
 org 0x7C00
 bits 16     ; bits directive, tells assembler to emit 16/32/64-bit code
 
+%define ENDL 0x0D, 0x0A
+
+start:
+  jmp main
+
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+puts:
+  ; save registers we will modify
+  push si
+  push ax
+
+.loop:
+  lodsb        ; loads next character in al
+  or al, al     ; verify if next character is null?
+  jz .done
+
+  mov ah, 0x0e	; call bios video interrupt
+  int 0x10
+
+  jmp .loop
+
+.done:
+  pop ax
+  pop si
+  ret
+
 main:
+  ; setup data segments
+  mov ax, 0		; can't write to ds/es directly
+  mov ds, ax
+  mov es, ax
+
+  ; setup stack
+  mov ss, ax
+  mov sp, 0x7C00	; stack grows downwards from where we are loaded in memory
+
+  ; print message
+  mov si, msg_hello
+  call puts
+
   hlt       ; halt the CPU. Can be started again via interrupts
 
 .halt:
   jmp .halt ; jump unconditionally to a location
+
+msg_hello: db 'mittOS v0.1', ENDL, 0
 
 ; db define byte
 ; times repeats instruction n times
